742a3eb089f5f853505f7743025e36449d287932
Mohsin/918 <mohsinquantum@gmail.com>
2023-12-26 11:09:51 +0530

    changes to getASNDetail API

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -2,4 +2,5 @@ using my.bookshop as my from '../db/data-model';
 
 service CatalogService {
     entity GetASNHeaderList as projection on my.GetASNHeaderList;
+    entity GetASNDetailList as projection on my.GetASNDetailList;
 }
--- srv/cat-service.js
+++ srv/cat-service.js
@@ -3,12 +3,22 @@ const axios = require('axios');
 
 module.exports = (srv) => {
 
-    const { GetASNHeaderList } = srv.entities;
+    const { GetASNHeaderList, GetASNDetailList } = srv.entities;
     
     srv.on('READ', GetASNHeaderList, async (req) => {
-        const {AddressCode} = req._queryOptions
-        //const AddressCode = 'PAI-01-03'
+        //const {AddressCode} = req._queryOptions
+        const AddressCode = 'PAI-01-03'
         const results = await getASNHeaderList(AddressCode);
+        if (!results) throw new Error('Unable to fetch ASN Header List.');
+        return results
+
+    });
+
+    srv.on('READ', GetASNDetailList, async (req) => {
+        //const {AddressCode, ASNNumber} = req._queryOptions
+        const AddressCode = 'PAI-01-03'
+        const ASNNumber = '22/01VASN/06648'
+        const results = await getASNDetailList(AddressCode, ASNNumber);
         if (!results) throw new Error('Unable to fetch ASN Header List.');
         return results
 
@@ -36,5 +46,29 @@ async function getASNHeaderList(AddressCode) {
     } catch (error) {
         console.error('Error in getASNHeaderList API call:', error);
         throw new Error('Unable to fetch ASN Header List.');
+    }
+}
+
+async function getASNDetailList(AddressCode, ASNNumber) {
+    try {
+        const response = await axios({
+            method: 'get',
+            url: `https://imperialauto.co:84/IAIAPI.asmx/GetASNDetailList?RequestBy='Manikandan'&AddressCode='${AddressCode}'&ASNNumber='${ASNNumber}'`,
+            headers: {
+                'Authorization': 'Bearer IncMpsaotdlKHYyyfGiVDg==',
+                'Content-Type': 'application/json'
+            },
+            data: {}
+        });
+
+        if (response.data && response.data.d) {
+            return JSON.parse(response.data.d);
+        } else {
+            console.error('Error parsing response:', response.data);
+            throw new Error('Error parsing the response from the API.');
+        }
+    } catch (error) {
+        console.error('Error in getASNDetailList API call:', error);
+        throw new Error('Unable to fetch ASN Detail List.');
     }
 }
