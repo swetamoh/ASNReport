827a1d2ae90d7ed78344bcaeb24d1683aeeaaaae
Rajesh Sehgal <rajeshsehgal@impauto.com>
2024-02-20 08:34:24 +0000

    vendor code change

--- app/asn/webapp/Component.js
+++ app/asn/webapp/Component.js
@@ -64,7 +64,7 @@ sap.ui.define([
                                 if( res.login_name[0] !==  res.email){
                                 sessionStorage.setItem('AddressCode', res.login_name[0]);
                                 }else {
-                                    sessionStorage.setItem('AddressCode', 'GKE-01-01');
+                                    sessionStorage.setItem('AddressCode', 'OKA-01-02');
                                 }
                             }
                         });
